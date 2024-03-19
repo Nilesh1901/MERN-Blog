450a21f396eaa250bae896f4f9200d9e2a56c34c
Nilesh Patel <knileshpatel.129@gmail.com>
2024-03-19 15:34:49 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: ture,
+    },
+    email: {
+      type: String,
+      required: ture,
+      unique: ture,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
