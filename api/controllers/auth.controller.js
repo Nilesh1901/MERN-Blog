c5212dad16a61860cb335a8c7cbaac7e4c65ccf6
Nilesh Patel <knileshpatel.129@gmail.com>
2024-03-21 14:59:26 +0530

    create signin api route and fix the errorhandler name

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import wrapAsync from "../utils/wrapAsync.js";
-import ErrorHandler from "../utils/error.js";
+import ExpressError from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = wrapAsync(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,7 +14,7 @@ export const signUp = wrapAsync(async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    return next(new ErrorHandler(400, "All fields are required"));
+    return next(new ExpressError(400, "All fields are required"));
   }
 
   const hashPassword = bcryptjs.hashSync(password, 12);
@@ -26,4 +27,28 @@ export const signUp = wrapAsync(async (req, res, next) => {
 
   await newUser.save();
   res.json("new user saved!");
+});
+
+export const signIn = wrapAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ExpressError(400, "All fields are required"));
+  }
+  const validUser = await User.findOne({ email });
+  if (!validUser) {
+    return next(new ExpressError(404, "User not found"));
+  }
+  const validPassword = bcryptjs.compareSync(password, validUser.password);
+  if (!validPassword) {
+    return next(new ExpressError(400, "Invalid Password"));
+  }
+
+  const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET);
+  const { password: pass, ...rest } = validUser._doc;
+  res
+    .status(200)
+    .cookie("access_token", token, {
+      httpOnly: true,
+    })
+    .json(rest);
 });
